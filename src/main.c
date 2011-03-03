b229c3511aa1e6fe0056f2cbc3df2d338e722f8b
David Zeuthen <davidz@redhat.com>
2011-03-03 13:44:01 -0500

    Don't use deprecated polkit API
    Signed-off-by: David Zeuthen <davidz@redhat.com>

--- src/main.c
+++ src/main.c
@@ -83,9 +83,11 @@ main (int argc, char **argv)
     }
 
   error = NULL;
-  if (!polkit_agent_register_listener (listener,
+  if (!polkit_agent_listener_register (listener,
+                                       POLKIT_AGENT_REGISTER_FLAGS_NONE,
                                        session,
                                        "/org/gnome/PolicyKit1/AuthenticationAgent",
+                                       NULL, /* GCancellable */
                                        &error))
     {
       g_printerr ("Cannot register authentication agent: %s\n", error->message);
