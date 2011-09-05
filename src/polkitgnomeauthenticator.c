1d019a66f9eaf6cd472eecfa38ce462a1b12906c
Matthias Clasen <mclasen@redhat.com>
2011-09-05 09:22:25 -0400

    Drop marshaller complications
    GLib provides a generic marshaller nowadays, lets use it.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=658256

--- src/polkitgnomeauthenticator.c
+++ src/polkitgnomeauthenticator.c
@@ -32,7 +32,6 @@
 
 #include "polkitgnomeauthenticator.h"
 #include "polkitgnomeauthenticationdialog.h"
-#include "polkitgnomemarshal.h"
 
 struct _PolkitGnomeAuthenticator
 {
@@ -138,7 +137,7 @@ polkit_gnome_authenticator_class_init (PolkitGnomeAuthenticatorClass *klass)
                                             0,                      /* class offset     */
                                             NULL,                   /* accumulator      */
                                             NULL,                   /* accumulator data */
-                                            _polkit_gnome_marshal_VOID__BOOLEAN_BOOLEAN,
+                                            NULL, /* use generic marshaller */
                                             G_TYPE_NONE,
                                             2,
                                             G_TYPE_BOOLEAN,
