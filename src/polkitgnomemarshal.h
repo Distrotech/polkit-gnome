95c4cccd999f61755b88e7aff10e7b8457b966aa
Greg Nietsky <gregory@distrotech.co.za>
2013-06-14 08:07:55 +0200

    Autotools

---
+++ src/polkitgnomemarshal.h
@@ -0,0 +1,20 @@
+
+#ifndef ___polkit_gnome_marshal_MARSHAL_H__
+#define ___polkit_gnome_marshal_MARSHAL_H__
+
+#include	<glib-object.h>
+
+G_BEGIN_DECLS
+
+/* VOID:BOOLEAN,BOOLEAN (./polkitgnomemarshal.list:1) */
+extern void _polkit_gnome_marshal_VOID__BOOLEAN_BOOLEAN (GClosure     *closure,
+                                                         GValue       *return_value,
+                                                         guint         n_param_values,
+                                                         const GValue *param_values,
+                                                         gpointer      invocation_hint,
+                                                         gpointer      marshal_data);
+
+G_END_DECLS
+
+#endif /* ___polkit_gnome_marshal_MARSHAL_H__ */
+
